ef5587bbb51894798cb1753bada89ffa53f9a996
iliyaM <iliyuskeisar@gmail.com>
2017-09-04 16:14:39 -0700

    spaced code

--- src/app/title-custom-pipe.pipe.ts
+++ src/app/title-custom-pipe.pipe.ts
@@ -8,6 +8,7 @@ export class TitleCustomPipePipe implements PipeTransform {
   transform(value: string, args?: any): any {
 	// Replaces string with only english characters.
 	let newString = value.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+	
 	// Capitalizes every first letter of a word
 	let capitalized = newString.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 	return capitalized;
